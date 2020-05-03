614e511ee5f556b8ba3369c07f3f5108b17d7852
Tony Minseok Kim <tonyminseokkim11@gmail.com>
2020-05-02 19:52:11 -0700

    Adding Heading and Nav components,

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 //Prop Types
 import PropType from 'prop-types';
 
+//importing Heading component
+import Heading from './Heading';
+
 // CSS
 import './../styles/UserProfile.css';
 
 // For styling Material UI components
 import { makeStyles } from '@material-ui/core/styles';
-
-// For User Profile page header profile picture
-import Avatar from '@material-ui/core/Avatar';
 
 // For User Info List section
 import List from "@material-ui/core/List";
@@ -19,9 +19,11 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
 // Icons for User Info List section
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import { green } from '@material-ui/core/colors';
 import EmailIcon from '@material-ui/icons/Email';
-import LockIcon from '@material-ui/icons/Lock';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import PersonIcon from '@material-ui/icons/Person';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 
 
 // a placeholder for user profile pic
@@ -31,105 +33,95 @@ import profilePic from './../image/logo.png';
 import { Z_BLOCK } from 'zlib';
 
 // This component will create a User Profile Page
-function UserProfile({profilePic, userName, name, userEmail, userBio}) {
+function UserProfile({profilePic, userName, isVerified, userEmail, userBio}) {
 
     return (
         <div className="UserProfilePage">
-            <UserHeader picture={profilePic} user={userName} />
-            <UserInfo user={userName} name={name} userEmail={userEmail} userBio={userBio} />
+            <Heading picture={profilePic} user={userName} />
+            <UserInfo userName={userName}  isVerified={isVerified} userEmail={userEmail} userBio={userBio} />
+            {/* <UserInfo /> */}
+
             <UserProfileBtnEdit />
             <UserProfileBtnBack />
         </div>
     )
 }
 
-// This component creates the user header containing the username and
-// a profile picture.
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    // marginLeft: '1em'
-    justifySelf: 'center'
-  },
-
-  large: {
-    width: theme.spacing(10),
-    height: theme.spacing(10)
-  }
-}));
-
-function UserHeader({profilePic, userName}) {
-    const classes = useStyles1();
-
-    return (
-        <div className="UserProfile_Header">
-            {/* $$$$ TIP $$$$ 
-                https://stackoverflow.com/questions/46066675/how-to-add-multiple-classes-in-material-ui-using-the-classes-props
-                
-                string interpolation:
-                <div className={`${this.props.classes.container} ${this.props.classes.spacious}`}>
-            */}
-            {/* <Avatar className={classes.root, classes.large} alt="Profile Picture" src={profilePic} /> */}
-            <Avatar className={`${classes.large} ${classes.root}`} alt="Profile Picture" src={profilePic} />
-
-            {/* <h2>{userName}</h2> */}
-            <h2>BBY TEAM 30</h2>
-        </div>
-     )
-}
 
 // styling for user info buttons
-// const useStyles2 = makeStyles((theme) => ({
-//     root: {
-//       width: '100%',
-//       maxWidth: 1500,
-//       backgroundColor: #424242,
-//       margin: '0 auto',
-//       marginTop: '2em'
-//     }
-//   }));
+const iconStyle = makeStyles((theme) => ({
+  root: {
+    // '& > userProfileIcon': {
+      marginRight: theme.spacing(1),
+    // },
+  },
+}));
+
 
 // This compontent creates a list of all the user's Info as buttons
-function UserInfo({userName, name, userEmail, userBio}) {
-    // const classes = useStyles2();
-  
-    return (
+function UserInfo({userName, isVerified, userEmail, userBio}) {
+
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     userName: "", 
+  //     isVerified: false,
+  //     userEmail: "",
+  //     userBio: "",
       
-      // <div className={classes.root}>
-      <div className="UserProfile_Info">
-        <List component="nav" 
-              aria-labelledby="nested-list-subheader" 
-              subheader={<ListSubheader component="div" id="nested-list-subheader">User Profile</ListSubheader>} 
-              className="UserProfile_Info"
-              >
+  //   };
+  // }
 
-          <ListItem button>
-            <ListItemText primary="User Name" secondary={userName} />
-          </ListItem>
 
-          <ListItem button>
-            <ListItemText primary="Name" secondary={name} />
-          </ListItem>
 
-          <ListItem button>
-            <ListItemText primary="Email" secondary={userEmail}   />
-            {/* <ListItemText primary="Email:" secondary="mkim265@my.bcit.ca" /> */}
-          </ListItem>
 
-          <ListItem button>
-            <ListItemText primary="Bio" secondary={userBio} />
-          </ListItem>
-          
-        </List>
-      </div>
-    );
+    
+
+      const classes = iconStyle();
+
+      return (
+      
+        // <div className={classes.root}>
+        <div className="UserProfile_Info">
+          <List component="nav" 
+                aria-labelledby="nested-list-subheader" 
+                subheader={<ListSubheader component="div" id="nested-list-subheader">User Profile</ListSubheader>} 
+                className="UserProfile_Info"
+                >
+  
+            <ListItem button>
+              <PersonIcon className={classes.root} style={{color: green[500]}} />
+              <ListItemText primary="Full Legal Name" secondary={userName} />
+            </ListItem>
+  
+            <ListItem button>
+              <VerifiedUserIcon className={classes.root} style={{color: green[500]}} />
+              <ListItemText primary="Verified" secondary={isVerified} />
+            </ListItem>
+  
+            <ListItem button>
+              <EmailIcon className={classes.root} style={{color: green[500]}} />
+              <ListItemText primary="Email" secondary={userEmail}   />
+              {/* <ListItemText primary="Email:" secondary="mkim265@my.bcit.ca" /> */}
+            </ListItem>
+  
+            <ListItem button>
+              <ImportContactsIcon className={classes.root} style={{color: green[500]}} />
+              <ListItemText primary="Bio" secondary={userBio} />
+            </ListItem>
+            
+          </List>
+        </div>
+      );
+    
 }
 
 UserInfo.PropType = {
   userName: PropType.string.isRequired,
-  age: PropType.number.isRequired,
-  name: PropType.string.isRequired,
-  email: PropType.string.isRequired,
-  bio: PropType.string.isRequired
+  isVerified: PropType.bool.isRequired,
+  userEmail: PropType.string.isRequired,
+  userBio: PropType.string.isRequired
 }
   
 function UserProfileBtnEdit() {
