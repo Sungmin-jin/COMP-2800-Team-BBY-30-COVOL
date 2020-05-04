952682852ee4234dbe4bdc427a7f4bab083831f2
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2020-05-04 21:02:08 +0900

    logout

--- client/src/components/WelcomePage.js
+++ client/src/components/WelcomePage.js
@@ -1,86 +1,138 @@
-import React, {Component} from 'react'
-
+import React, { Component, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
 //Importing Footer Component
 import Footer from './Footer';
 
 //Prop Types
 import PropTypes from 'prop-types';
 
-//Material UI components    
-import Button from "@material-ui/core/Button";
+//Material UI components
+import Button from '@material-ui/core/Button';
 
 //CSS
 import './../styles/WelcomePage.css';
 
 import logoIMG from './../image/logo.png';
 
+//LOGOUT
+import { connect } from 'react-redux';
+import { logout } from '../actions/auth';
+
 // This component will create a User Welcome Page
-function WelcomePage({logo, userName}) {
-    return (
-        <div>
-            <WelcomePageLogo logo ={logo} />
-            <WelcomePageName name ={userName} />
-            <WelcomePageVolunteerBtn />
-            <WelcomePageProfileBtn />
-            <WelcomePageLogout />
-            <Footer />
-        </div>
-    );
+function WelcomePage({ logo, userName, isAuthenticated, logout }) {
+  if (!isAuthenticated) {
+    return <Redirect to='/' />;
+  }
+  return (
+    <div>
+      <WelcomePageLogo logo={logo} />
+      <WelcomePageName name={userName} />
+      <WelcomePageVolunteerBtn />
+      <WelcomePageProfileBtn />
+      <WelcomePageLogout logout={logout} />
+      <Footer />
+    </div>
+  );
 }
+
+// const WelcomePage = ({
+//   logo,
+//   userName,
+//   auth: { isAuthenticated, loading },
+//   logout,
+// }) => {
+//   if (isAuthenticated) {
+//     return <Redirect to='/welcome' />;
+//   }
+//   return (
+//     <div>
+//       <WelcomePageLogo logo={logo} />
+//       <WelcomePageName name={userName} />
+//       <WelcomePageVolunteerBtn />
+//       <WelcomePageProfileBtn />
+//       <WelcomePageLogout logout={logout} />
+//       <Footer />
+//     </div>
+//   );
+// };
 
 WelcomePage.propTypes = {
-    logo: PropTypes.string.isRequired,
-    userName: PropTypes.string.isRequired  
-}
+  logo: PropTypes.string.isRequired,
+  userName: PropTypes.string.isRequired,
+};
 
 //This component is the logo of the User Welcome Page
-function WelcomePageLogo ({logo}) {
-    return (
-        <img className="logo2" alt="App Logo" src={logoIMG}></img>
-    )
+function WelcomePageLogo({ logo }) {
+  return <img className='logo2' alt='App Logo' src={logoIMG}></img>;
 }
 
 WelcomePageLogo.propTypes = {
-    logo: PropTypes.string.isRequired 
-}
+  logo: PropTypes.string.isRequired,
+};
 
-//This component is the Welcome message to the user 
-function WelcomePageName ({userName}) {
-    return (
-        // <p className="WelcomePageName" >Welcome {userName}</p>
-        <p className="WelcomePageName" >Welcome Justin</p>
-    )
+//This component is the Welcome message to the user
+function WelcomePageName({ userName }) {
+  return (
+    // <p className="WelcomePageName" >Welcome {userName}</p>
+    <p className='WelcomePageName'>Welcome Justin</p>
+  );
 }
 
 WelcomePageName.propTypes = {
-    userName: PropTypes.string.isRequired 
-}
+  userName: PropTypes.string.isRequired,
+};
 
 //This component is the buttons on the User Welcome page
 function WelcomePageVolunteerBtn() {
-    return (
-        <div className="con-1">
-            <a className="WelcomePageBtn" href="">Volunteer</a>
-        </div>
-    )
+  return (
+    <div className='con-1'>
+      <a className='WelcomePageBtn' href=''>
+        Volunteer
+      </a>
+    </div>
+  );
 }
 
 //This component is the buttons on the User Welcome page
 function WelcomePageProfileBtn() {
-    return (
-        <div className="con-1">
-            <a className="WelcomePageBtn" href="./userprofile">Profile</a> 
-        </div>
-    )
+  return (
+    <div className='con-1'>
+      <a className='WelcomePageBtn' href='./userprofile'>
+        Profile
+      </a>
+    </div>
+  );
 }
 
 //this component is the logout button on the User Welcome page
-function WelcomePageLogout() {
-    return (
-        <div>
-            <Button>Logout</Button>
-        </div>
-    )
-}
+const WelcomePageLogout = ({ logout }) => {
+  return (
+    <Fragment>
+      <div>
+        <a onClick={logout} href='/'>
+          Logout
+        </a>
+      </div>
+    </Fragment>
+  );
+};
 
-export default WelcomePage;
+// function WelcomePageLogout({ auth: logout }) {
+//   return (
+//     <div>
+//       <a onClick={logout}>Logout</a>
+//     </div>
+//   );
+// }
+
+WelcomePage.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(WelcomePage);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,58 +1,64 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const {
-    check,
-    validationResult
-} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
-const Profile = require('../../models/Profile')
-const User = require('../../models/User')
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile/me
 // @desc  Get current user profile
 // @access Private
 router.get('/me', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({
-            user: req.user.id
-        }).populate('user', ['name', 'avatar']);
-        if (!profile) {
-            return res.status(400).json({
-                msg: 'There is no profile for this user'
-            })
-        }
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+    if (!profile) {
+      return res.status(400).json({
+        msg: 'There is no profile for this user',
+      });
     }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 // @route Post api/profile
 // @desc  Create or update user profile
 // @access Private
-router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), check('skills', 'Skills is required').not().isEmpty()]], async (req, res) => {
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({
-            errors: errors.array()
-        });
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
 
     const {
-        company,
-        website,
-        location,
-        bio,
-        status,
-        githubusername,
-        skills,
-        youtube,
-        facebook,
-        twitter,
-        instagram,
-        linkedin
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
     } = req.body;
 
     // Build profile object
@@ -65,11 +71,11 @@ router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), 
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-        profileFields.skills = skills.split(',').map(skill => skill.trim());
+      profileFields.skills = skills.split(',').map((skill) => skill.trim());
     }
 
     //Build social object
-    profileFields.social = {}
+    profileFields.social = {};
     if (youtube) profileFields.social.youtube = youtube;
     if (twitter) profileFields.social.twitter = twitter;
     if (facebook) profileFields.social.facebook = facebook;
@@ -77,94 +83,97 @@ router.post('/', [auth, [check('status', 'Status is required').not().isEmpty(), 
     if (instagram) profileFields.social.instagram = instagram;
 
     try {
-        let profile = await Profile.findOne({
-            user: req.user.id
-        });
+      let profile = await Profile.findOne({
+        user: req.user.id,
+      });
 
-        if (profile) {
-            //update
-            profile = await Profile.findOneAndUpdate({
-                user: req.user.id
-            }, {
-                $set: profileFields
-            }, {
-                new: true
-            });
-            return res.json(profile);
-        }
+      if (profile) {
+        //update
+        profile = await Profile.findOneAndUpdate(
+          {
+            user: req.user.id,
+          },
+          {
+            $set: profileFields,
+          },
+          {
+            new: true,
+          }
+        );
+        return res.json(profile);
+      }
 
-        //Create
-        profile = new Profile(profileFields);
+      //Create
+      profile = new Profile(profileFields);
 
-        await profile.save();
-        res.json(profile);
+      await profile.save();
+      res.json(profile);
     } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
-});
+  }
+);
 
 // @route Get api/profile
 // @desc  Get all profiles
 // @access public
 router.get('/', async (req, res) => {
-    try {
-        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
-        res.json(profiles);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
-
 
 // @route Get api/profile/:user_id
 // @desc  Get profile by user ID
 // @access public
 router.get('/user/:user_id', async (req, res) => {
-    try {
-        const profile = await Profile.findOne({
-            user: req.params.user_id
-        }).populate('user', ['name', 'avatar']);
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', ['name', 'avatar']);
 
-        if (!profile) return res.status(400).json({
-            msg: 'Profile not found'
-        });
+    if (!profile)
+      return res.status(400).json({
+        msg: 'Profile not found',
+      });
 
-        res.json(profile);
-    } catch (err) {
-        console.error(err.message);
-        if (err.kind == 'ObjectId') {
-            return res.status(400).json({
-                msg: 'Profile not found'
-            });
-        }
-        res.status(500).send('Server Error');
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({
+        msg: 'Profile not found',
+      });
     }
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route DELETE api/profile
 // @desc  Delete profile, user
 // @access private
 router.delete('/', auth, async (req, res) => {
-    try {
-        // Remove profile
-        await Profile.findOneAndRemove({
-            user: req.user.id
-        });
-        // Remove user
-        await User.findOneAndRemove({
-            _id: req.user.id
-        })
-        res.json({
-            msg: 'User deleted'
-        });
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-})
-
-
+  try {
+    // Remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id,
+    });
+    // Remove user
+    await User.findOneAndRemove({
+      _id: req.user.id,
+    });
+    res.json({
+      msg: 'User deleted',
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
