82ea66078df31399df308955cb6080ca97bd06d8
Justin Payne <justinthecore@gmail.com>
2020-05-07 14:44:37 -0700

    updated data for this attempt

--- client/src/data.js
+++ client/src/data.js
@@ -1,46 +1,46 @@
 export default [
     {
       pics: [
-        "https://images.unsplash.com/photo-1520024146169-3240400354ae?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+        "https://images.unsplash.com/photo-1506919258185-6078bba55d2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=815&q=80"
       ],
-      name: "Heather",
+      name: "Marius",
       age: 32,
       text:
-        "My nickname is Gilette because I'm the best a man can get. Also I will cut you."
+        "I could do laps around all these people behind me. Easy."
     },
     {
       pics: [
-        "https://images.unsplash.com/photo-1522263842439-347f062b8475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
       ],
-      name: "Chloe",
-      age: 18,
+      name: "Ivana",
+      age: 26,
       text:
-        "I keep calling the plumber round, and he keeps fixing stuff. Where am I going wrong?"
+        "This coronavirus is really killing my vibe. Let me out side. I gotta got for a ride."
     },
     {
       pics: [
-        "https://images.unsplash.com/photo-1539758462369-43adaa19bc1f?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
+        "https://images.unsplash.com/photo-1530268729831-4b0b9e170218?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
       ],
-      name: "Helen",
-      age: 19,
-      text: "Aspiring MILF"
-    },
-    {
-      pics: [
-        "https://images.unsplash.com/photo-1514924801778-1db0aba75e9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
-      ],
-      name: "Savannah",
+      name: "Harps",
       age: 29,
-      text: "A little known fact is that I cover about 40% of Africa."
+      text: "This was part of a bunch of headshots I did for a small digital agency in Hackney"
     },
     {
       pics: [
-        "https://images.unsplash.com/photo-1456885284447-7dd4bb8720bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80",
-        "https://images.unsplash.com/photo-1532635270-c09dac425ca9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+        "https://images.unsplash.com/photo-1506277886164-e25aa3f4ef7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
       ],
-      name: "Jane",
-      age: 22,
+      name: "Charles",
+      age: 37,
+      text: "I am the best footballer in all of Africa."
+    },
+    {
+      pics: [
+        "https://images.unsplash.com/photo-1492446845049-9c50cc313f00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+        "https://images.unsplash.com/photo-1492447046925-35d93a19f006?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+      ],
+      name: "Brock",
+      age: 28,
       text:
-        "On the first date I will carve our initials in a tree. It's the most romantic way to let you know I have a knife."
+        "Live life on the board. Skateboard"
     }
   ];
