409ba8d444663368674f699348f3c012bea81948
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2020-05-23 04:10:40 +0900

    comments

--- server.js
+++ server.js
@@ -31,11 +31,13 @@ if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  //send all routes
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+//localhost port
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
