cb2d0a0ec406eee8ad68028b0c53aa64372fcd26
Jason <56404503+Sungmin-jin@users.noreply.github.com>
2020-05-23 02:48:42 +0900

    comment

--- server.js
+++ server.js
@@ -21,20 +21,21 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-
-
 //Server static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
+  // Send all routes
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
+//local host port
 const PORT = process.env.PORT || 8000;
 
+//run the server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
